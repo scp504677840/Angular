561d529edc52681531b01c28d33218f4ade8cf60
Song <504677840@qq.com>
2017-08-26 10:29:34 +0800

    模板驱动的表单
    模板驱动的表单

---
+++ Form/Template-driven forms/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {TemplateFormComponent} from './template-form/template-form.component';
+import {RegisterComponent} from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
